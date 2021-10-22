cc06cc81dbdda559e93e8b24d082bc7fd1dfbb93
VictorBenitesReyna <wow_elfo09@outlook.com>
2021-10-22 18:01:31 -0500

    segundo avance

--- src/main/java/pe/edu/upc/controller/UsuarioController.java
+++ src/main/java/pe/edu/upc/controller/UsuarioController.java
@@ -1,5 +1,55 @@
 package pe.edu.upc.controller;
 
-public class UsuarioController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
+import pe.edu.upc.entities.Usuario;
+import pe.edu.upc.serviceinterfaces.IUsuarioService;
+
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	@Autowired
+	private IUsuarioService uService;
+	@GetMapping("/new")
+	public String newUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "usuario/usuario";
+	}
+	@GetMapping("/list")
+	public String listUsuarios(Model model) {
+		try {
+			model.addAttribute("usuario", new Usuario());
+			model.addAttribute("listaUsuarios", uService.list());
+		} catch (Exception e) {
+			model.addAttribute("error", e.getMessage());
+		}
+		return "usuario/listUsuarios";
+	}
+	@PostMapping("/save")
+	public String saveUsuario(@Validated Usuario usuario, BindingResult result, Model model, SessionStatus status)
+			throws Exception {
+		if (result.hasErrors()) {
+			return "usuario/usuario";
+		} else {
+			int rpta = uService.insert(usuario);
+			if (rpta > 0) {
+				model.addAttribute("mensaje", "Ya existe");
+				return "usuario/usuario";
+			} else {
+				model.addAttribute("mensaje", "Se guardó correctamente");
+				status.setComplete();
+			}
+		}
+		model.addAttribute("usuario", new Usuario());
+		return "redirect:/usuarios/list";
+	}
 }
--- src/main/java/pe/edu/upc/entities/Usuario.java
+++ src/main/java/pe/edu/upc/entities/Usuario.java
@@ -1,5 +1,78 @@
 package pe.edu.upc.entities;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
 
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUsuario;
+	@Pattern(regexp = "[^!\"#$%&'()*+,-./:;<=>?@^_`{|}~]+", message = "El nombre del Usuario no puede contener un caracter especial")
+	@Pattern(regexp = "[^0-9]+", message = "El nombre del Usuario no puede contener un número")
+	@Column(name = "nombreUsuario",length=35,nullable = false)
+	private String nombreUsuario;
+	@Pattern(regexp = "[^!\"#$%&'()*+,-./:;<=>?@^_`{|}~]+", message = "El Apellido del Usuario no puede contener un caracter especial")
+	@Pattern(regexp = "[^0-9]+", message = "El Apellido del Usuario no puede contener un número")
+	@Column(name = "apellidoUsuario", nullable = false, length = 20)
+	private String apellidoUsuario;
+	@Column(name = "password", nullable = false, length = 20)
+	private String password;
+	@Column(name = "nickname", nullable = false, length = 20)
+	private String nickname;
+	
+	
+	public Usuario(int idUsuario,
+			@Pattern(regexp = "[^!\"#$%&'()*+,-./:;<=>?@^_`{|}~]+", message = "El nombre del Usuario no puede contener un caracter especial") @Pattern(regexp = "[^0-9]+", message = "El nombre del Usuario no puede contener un número") String nombreUsuario,
+			@Pattern(regexp = "[^!\"#$%&'()*+,-./:;<=>?@^_`{|}~]+", message = "El Apellido del Usuario no puede contener un caracter especial") @Pattern(regexp = "[^0-9]+", message = "El Apellido del Usuario no puede contener un número") String apellidoUsuario,
+			String password, String nickname) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nombreUsuario = nombreUsuario;
+		this.apellidoUsuario = apellidoUsuario;
+		this.password = password;
+		this.nickname = nickname;
+	}
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getApellidoUsuario() {
+		return apellidoUsuario;
+	}
+	public void setApellidoUsuario(String apellidoUsuario) {
+		this.apellidoUsuario = apellidoUsuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
 }
