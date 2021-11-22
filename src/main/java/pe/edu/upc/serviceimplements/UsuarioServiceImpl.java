0068b913d74df4bc1189e94f2ab27039cce8d726
VictorBenitesReyna <wow_elfo09@outlook.com>
2021-11-21 23:58:07 -0500

    correcciones heroku

--- src/main/java/pe/edu/upc/serviceimplements/UsuarioServiceImpl.java
+++ src/main/java/pe/edu/upc/serviceimplements/UsuarioServiceImpl.java
@@ -2,7 +2,6 @@ package pe.edu.upc.serviceimplements;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/pe/edu/upc/serviceinterfaces/IVideoJuegoService.java
+++ src/main/java/pe/edu/upc/serviceinterfaces/IVideoJuegoService.java
@@ -1,7 +1,6 @@
 package pe.edu.upc.serviceinterfaces;
 
 import java.util.List;
-import java.util.Optional;
 
 import pe.edu.upc.entities.VideoJuego;
 
